d85f02f51edfc5159e9b7a14f32194367c146b43
krxzy-k <kabelopheeha55@gmail.com>
2026-04-29 12:02:26 +0200

    Added a function that generates random numbers

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function getRandomCard() {
-    return 5
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber === 1) {
+        return 11
+    } else if (randomNumber > 10) {
+        return 10
+    } else {
+        return randomNumber
+    }
 }
 
 function startGame() {
